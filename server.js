3cf051228e94f04d569f5397e206c6473185cabe
Sam Jones <sam@samljones.com>
2016-03-06 17:08:23 -0500

    prod deployment of bundled assets using express instead of webpack dev server

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
-
 var env = process.env.NODE_ENV;
-var hot = (env === 'development') ? true : false;
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: hot,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
+if (env === 'development') {
+  var webpack = require('webpack');
+  var WebpackDevServer = require('webpack-dev-server');
+  var config = require('./webpack.config');
 
-  console.log('Listening at localhost:3000');
-});
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  }).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log('Listening at localhost:3000');
+  });
+} else {
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname + '/dist'));
+
+  var port = process.env.PORT || 3000;
+  app.listen(port, function() {
+      console.log('Our app is running on http://localhost:' + port);
+  });
+}
